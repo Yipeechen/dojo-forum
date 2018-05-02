fe496c462e8a10eda3a2bf55670932cae0679629
Yipeechen <yipeechen0418@gmail.com>
2018-05-02 10:48:08 +0800

    add post index & new & show & create action

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ end
 gem 'devise'
 gem 'bootstrap-sass', '~> 3.3.7'
 gem 'jquery-rails'
+gem 'carrierwave'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.6'
 # Use sqlite3 as the database for Active Record
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,32 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "post was successfully created"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "post was failed to create"
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :image)
   end
 end
+
