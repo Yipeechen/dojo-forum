76c9806e0dbed9fabf731d21a5b5c7bca3122d9f
Yipeechen <yipeechen0418@gmail.com>
2018-05-06 22:22:44 +0800

    admin can change user's role type on user list

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,25 @@
+class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin
+
+  def index
+    @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_users_path
+      flash[:notice] = "Role was successfully updated"
+    else
+      @users = User.all
+      render :index
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -26,5 +26,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :categories
     root "categories#index"
+
+    resources :users, only: [:index, :update]
   end
 end
