34a8cbcab9552d03db58e3a0febe4e7641ea084b
Yipeechen <yipeechen0418@gmail.com>
2018-05-08 23:05:06 +0800

    change replies routes

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -8,27 +8,21 @@ class RepliesController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
     @reply = Reply.find(params[:id])
 
     if current_user.admin? || current_user == @reply.user
       @reply.destroy
-      redirect_to post_path(@post)
+      redirect_to post_path(@reply.post)
     end
   end
 
   def edit
-    @post = Post.find(params[:post_id])
     @reply = Reply.find(params[:id])
-
-    render :json => {:post => @post , :replies => @post.replies}
   end
 
   def update
-    @post = Post.find(params[:post_id])
     @reply = Reply.find(params[:id])
     @reply.update_attributes(reply_params)
-    render :json => {:post => @post , :replies => @post.replies}
   end
 
   private
