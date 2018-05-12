65ce3e5dd9280fbda372e27c4f9f8d899b06f7d0
Yipeechen <yipeechen0418@gmail.com>
2018-05-12 22:31:32 +0800

    add index action for post api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::PostsController < ApiController
+
+  def index
+    @posts = Post.where("status = ? AND authority = ?", true, "All")
+
+    if user_signed_in?
+      @posts = @posts.check_authority(current_user)
+    end
+    render json: @posts
+  end
 end
