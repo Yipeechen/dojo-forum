b0d8cb37b51d9ca75fdcb43da508dfbaf060a778
Yipeechen <yipeechen0418@gmail.com>
2018-05-14 11:35:51 +0800

    user list order by created_at

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_admin
 
   def index
-    @users = User.all
+    @users = User.all.order(created_at: :asc)
   end
 
   def update
