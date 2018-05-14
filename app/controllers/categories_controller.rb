48298891f743139cf2cbe70fc7b4bd07cbe12007
Yipeechen <yipeechen0418@gmail.com>
2018-05-14 11:20:34 +0800

    post list order by created_at

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   def show
     @categories = Category.all
     @category = Category.find(params[:id])
-    @posts = @category.posts.where('status = ?', true).page(params[:page]).per(20)
+    @posts = @category.posts.where('status = ?', true).order(created_at: :asc).page(params[:page]).per(20)
     if user_signed_in?
       @posts = @posts.check_authority(current_user)
     end
